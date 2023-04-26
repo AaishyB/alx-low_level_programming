d9a0413cae8ec6211f5f17947439d35cb1bd72e2
AaishyB <aaishaagherdien16@gmail.com>
2023-04-26 12:44:56 -0700

    print_alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+    char c = 'a';
+
+    while (c <= 'z')
+    {
+        putchar(c);
+        c++;
+    }
+    putchar('\n');
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
