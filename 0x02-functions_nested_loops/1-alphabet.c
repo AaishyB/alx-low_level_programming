dbed686dbb4bb8a8301c31b15d9dc28ef0db5dd6
AaishyB <aaishaagherdien16@gmail.com>
2023-04-26 15:09:48 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,12 @@
  */
 void print_alphabet(void)
 {
-    char c = 'a';
+	char c = 'a';
 
-    while (c <= 'z')
-    {
-        putchar(c);
-        c++;
-    }
-    putchar('\n');
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+		}	
+	putchar('\n');
 }
